4932786f95184519a38b0f1a68af5dee1a5c6145
wertyuiopsdfghjklxcvbn <alexeypavlov.m@gmail.com>
2021-07-31 19:54:00 +0300

    commit num 15

--- scripts/python/build.py
+++ scripts/python/build.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import subprocess
+
 
 pathToBuildDirectory = 'build'
 
@@ -11,8 +13,11 @@ except FileExistsError:
 os.chdir(pathToBuildDirectory)
 
 if sys.platform == 'win32':
-    print(os.popen('cmake -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=Debug .. && ninja').read())
+    print(subprocess.run(['cmake', '-GNinja', '-DCMAKE_EXPORT_COMPILE_COMMANDS=on', '-DCMAKE_BUILD_TYPE=Debug', './..'], check = True))
+    print(subprocess.run(['ninja', '-d', 'stats'], shell=True))
 
+#process = subprocess.Popen( ['ninja', '-d', 'stats'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+#print(process.wait())
 
 
